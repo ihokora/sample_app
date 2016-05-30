f2211bc222aa5de8e535fcdfd5565f93fdb1455a
Ihor Sikora <sikoraigo@gmail.com>
2016-05-30 21:25:22 +0300

    Make a basic User model (icluding secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+  
   validates :password, presence: true, length: { minimum: 6 }
 
 
